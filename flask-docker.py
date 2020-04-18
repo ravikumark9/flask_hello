934a9c5c6459420163e4a1dc2bc01bfac76e7b88
ravi <ravikumark9@gmail.com>
2020-04-18 16:31:02 +0000

    a1:only hello available

--- flask-docker.py
+++ flask-docker.py
@@ -7,8 +7,4 @@ def hello_world():
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0',port=int("80"))
-<<<<<<< HEAD
 print("hello")
-=======
-print("hello1")
->>>>>>> e28ca849144a7b6884bfd79f3d64425ea5a50d20
